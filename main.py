7039a3334c6935fb7eceb23962c1e04517e37891
sergiogblanco1-lab <sergiog.blanco1@gmail.com>
2026-03-13 13:21:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-return {"status": "ETERNA backend alive"}
+    return {"status": "ETERNA backend alive"}
 
 @app.get("/crear-eterna")
 def crear_eterna():
-return {
-"ok": True,
-"message": "Tu ETERNA ha comenzado a crearse",
-"next_step": "Subir 6 fotos y escribir 3 frases"
+    return {
+    "ok": True,
+    "message": "Tu ETERNA ha comenzado a crearse",
+    "next_step": "Subir 6 fotos y escribir 3 frases"
 }
 
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 10000))
-uvicorn.run(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
