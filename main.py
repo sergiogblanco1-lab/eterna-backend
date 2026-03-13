eb1d9b9bbea21e33ee9adc42531d058a6c4f3e1e
sergiogblanco1-lab <sergiog.blanco1@gmail.com>
2026-03-13 13:28:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
+from typing import List
 import uvicorn
 import os
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"status": "ETERNA backend alive"}
 
-@app.get("/crear-eterna")
-def crear_eterna():
+
+@app.post("/crear-eterna")
+async def crear_eterna(
+    nombre: str = Form(...),
+    email: str = Form(...),
+    frase1: str = Form(...),
+    frase2: str = Form(...),
+    frase3: str = Form(...),
+    fotos: List[UploadFile] = File(...)
+):
     return {
-    "ok": True,
-    "message": "Tu ETERNA ha comenzado a crearse",
-    "next_step": "Subir 6 fotos y escribir 3 frases"
-}
+        "ok": True,
+        "message": "Tu ETERNA ha sido recibida",
+        "nombre": nombre,
+        "email": email,
+        "frases": [frase1, frase2, frase3],
+        "numero_fotos": len(fotos)
+    }
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
