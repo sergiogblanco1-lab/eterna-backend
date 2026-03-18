167797d4b98038826727ebb6526a39e99edfa945
sergiogblanco1-lab <sergiog.blanco1@gmail.com>
2026-03-18 19:26:31 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,10 +4,15 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 DATABASE_URL = "sqlite:///./eterna.db"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
