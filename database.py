aa28f013f0090d5c3a1983409d0c3373ac422cc5
sergiogblanco1-lab <sergiog.blanco1@gmail.com>
2026-03-20 00:49:49 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./eterna.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./eterna_lab.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
